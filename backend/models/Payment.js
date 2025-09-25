279867a1616e949954a0456a3512b69e972d23f6
Seu Nome <licitaquiassessoriaoficial@gmail.com>
2025-09-25 17:52:56 -0300

    fix: corrigir importação da instância sequelize em Payment.js

---
+++ backend/models/Payment.js
@@ -0,0 +1,18 @@
+// models/Payment.js
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./index');
+
+const Payment = sequelize.define('Payment', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  userId: { type: DataTypes.INTEGER, allowNull: false },
+  asaasPaymentId: { type: DataTypes.STRING, allowNull: false },
+  value: { type: DataTypes.FLOAT, allowNull: false },
+  status: { type: DataTypes.STRING, allowNull: false },
+  dueDate: { type: DataTypes.DATE, allowNull: false },
+  paymentDate: { type: DataTypes.DATE, allowNull: true },
+  method: { type: DataTypes.STRING, allowNull: true },
+}, {
+  timestamps: true
+});
+
+module.exports = Payment;
